4c891b162a8d7f47d7454e104bb006c4b30d8636
ΔK <itsahmed1o9@gmail.com>
2024-04-22 13:22:35 +0300

    init

---
+++ objects/column.py
@@ -0,0 +1,20 @@
+import pygame
+from random import randint
+
+pygame.init()
+
+class Column(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = ''
+        self.rect = pygame.Rect(450, 300, 20, 100)
+
+    def change_height():
+        # TODO: AFTER BALL PASSES THROUGH GAP
+        return
+
+    def destroy():
+        Column.kill() # ~
+
+    def update():
+        return
+    
---
+++ objects/player.py
@@ -0,0 +1,20 @@
+import pygame
+from random import randint
+
+pygame.init()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = ''
+        self.rect = pygame.Rect(430,150,30,30)
+
+    def change_height():
+        # TODO: AFTER BALL PASSES THROUGH GAP
+        return
+
+    def destroy():
+        Player.kill() # ~
+
+    def update():
+        return
+    
