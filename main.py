5a7825d6aab8cafae234b73dc6e9231eb74fae17
ΔK <itsahmed1o9@gmail.com>
2024-04-25 15:44:38 +0300

    added obstacle timers

--- main.py
+++ main.py
@@ -15,88 +15,77 @@ class Game:
         self.clock = pygame.time.Clock()
         self.screen_controller = 0  # -1: GAME OVER, 0: MENU, 1: ACTIVE GAME
 
-        # sprite groups
+        # timer
+        self.meteor_timer = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.meteor_timer, 7800)
+        # TODO: tower_timer
+
+        # sprite groups & setup
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
-        # sprites
         self.player = Player(self.all_sprites)
-        self.planet = Planet(self.all_sprites)
-        self.tower = Tower(self.all_sprites)
-        self.meteor = Meteor(self.all_sprites)
-
-        # timer
-        # self.obstacle_timer = pygame.USEREVENT + 1
-        # pygame.time.set_timer(self.obstacle_timer, 1000)
+        self.planet = Planet([self.all_sprites, self.collision_sprites])
+        Tower([self.all_sprites, self.collision_sprites])
+        # Meteor([self.all_sprites, self.collision_sprites])
 
         # fonts
         self.header_font = pygame.font.Font('assets/fonts/Poppins-Light.ttf', 64)
         self.body_font = pygame.font.Font('assets/fonts/Poppins-Light.ttf', 32)
         self.lives = 3
         self.score = 0
+        self.start_time = 0
+
+        # title
+        self.title_img = pygame.image.load('assets/images/title_logo.png').convert_alpha()
+        self.title_img = pygame.transform.smoothscale_by(self.title_img, 0.5)
+        self.title_rect = self.title_img.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/6))
 
         # text
-        self.game_over_surf = self.header_font.render("GAME OVER", False, '#FFFFFF')
+        self.game_over_surf = self.header_font.render("GAME OVER", True, '#FFFFFF')
         self.game_over_rect = self.game_over_surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/6))
 
-        self.play_msg_surf = self.body_font.render("Press 'SPACE' to PLAY", False, '#FFFFFF')
+        self.prev_score_surf = self.body_font.render("SCORE", True, '#FFFFFF')
+        self.prev_score_rect = self.prev_score_surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT * 5/11 - 30))
+
+        self.play_msg_surf = self.body_font.render("Press 'SPACE' to PLAY", True, '#FFFFFF')
         self.play_msg_rect = self.play_msg_surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT*5/6))
 
         # audio
         # TODO: BG music and score sfx
 
     def collisions(self):
-        return
+        if pygame.sprite.spritecollide(self.player, self.collision_sprites, False, pygame.sprite.collide_mask) or self.player.rect.top > SCREEN_HEIGHT or self.player.rect.bottom < 0 or self.player.rect.left > SCREEN_WIDTH+20 or self.player.rect.right < -20:
+            print("Collision!")
+            self.lose_life()
     
     def add_score(self):
         self.score += 1
 
     def lose_life(self):
-        self.lives -= 1
+        if self.lives != 0:
+            self.lives -= 1
+            self.player.respawn()
+        else:
+            self.screen_controller = -1
 
     def reset_stats(self):
-        self.score = 0
         self.lives = 3
+        self.start_time = pygame.time.get_ticks()
 
     def display_score(self, x, y):
-        score_surf = self.header_font.render(f'{self.score}', False, (255,255,255))
+        if self.screen_controller == 1:
+            self.score = (pygame.time.get_ticks() - self.start_time) // 1000
+        score_surf = self.header_font.render(f'{self.score}', True, (255,255,255))
         score_rect = score_surf.get_rect(center = (x, y))
         self.screen.blit(score_surf, score_rect)
         
     def display_lives(self, x, y):
-        lives_surf = self.header_font.render(f'x {self.lives}', False, (255,255,255))
+        lives_surf = self.header_font.render(f'x {self.lives}', True, (255,255,255))
         lives_rect = lives_surf.get_rect(center = (x, y))
         self.screen.blit(lives_surf, lives_rect)
 
     def run(self):
-        # Attributes for orbital motion
-        # player_angle = 0
-        # angle_incr = 0.016 # angle is incremented by this value each frame
-        # player_speed_multiplier = 5 # the speed of the orbital
-        # player_gravity = 0.01 # the pull towards the center
-
-        # player_x = SCREEN_WIDTH/2 - 20
-        # player_y = 150
-        # player = pygame.Rect(player_x, player_y, 30, 30)
-
-        # Interface 
-        # score = 0
-        # lives = 3
-
-        # header_font = pygame.font.Font('assets/fonts/Poppins-Light.ttf', 64)
-        # body_font = pygame.font.Font('assets/fonts/Poppins-Light.ttf', 32)
-
-        # screen_controller = 0
-        # GAME OVER screen -> -1
-        # MENU screen -> 0
-        # ACTIVE GAME screen -> 1
-
-        
-        # Timer
-        # start_timer = pygame.USEREVENT + 1
-        # pygame.time.set_timer(start_timer, 800)
-        # obstacle_timer = pygame.USEREVENT + 2
-        
         last_time = time.time()
         running = True
         while running:
@@ -111,17 +100,17 @@ class Game:
                 if self.screen_controller==1:
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         print(event.pos)
+                    if event.type == self.meteor_timer:
+                        Meteor([self.all_sprites, self.collision_sprites])
                 else:
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_SPACE:
-                            lives = 3
-                            self.score = 0
-                            # player_gravity = 0
+                            self.reset_stats()
                             self.screen_controller = 1
+                            self.player.reset_speed()
+                            self.player.respawn()
                         elif event.key == pygame.K_ESCAPE and self.screen_controller==-1:
                             print('esc')
-                            lives = 3
-                            self.score = 0
                             self.screen_controller = 0
             
             if self.screen_controller == 1:
@@ -132,11 +121,12 @@ class Game:
 
                 # Draw sprites
                 self.all_sprites.update(dt)
+                self.collisions()
                 self.all_sprites.draw(self.screen)
 
                 # Draw surfaces
                 self.display_score(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-                self.display_lives(SCREEN_WIDTH-80, 40)
+                self.display_lives(SCREEN_WIDTH/10 * 9, SCREEN_HEIGHT/20)
 
             elif self.screen_controller == 0:
                 # MENU SCREEN
@@ -144,6 +134,7 @@ class Game:
                 self.screen.fill('Black')
                 outer_circle = pygame.draw.circle(self.screen, ((255,255,255)), (360, 450), 132)
                 inner_circle = pygame.draw.circle(self.screen, ((0,0,0)), (360, 450), 130)
+                self.screen.blit(self.title_img, self.title_rect)
                 self.screen.blit(self.play_msg_surf, self.play_msg_rect)
                 if self.score!= 0: self.display_score(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -151,9 +142,14 @@ class Game:
                 # GAME OVER SCREEN
                 self.screen.fill('Black')
                 # TODO: Add some idle animation
-                outer_circle = pygame.draw.circle(self.screen, ((255,255,255)), (360, 450), 132)
-                inner_circle = pygame.draw.circle(self.screen, ((0,0,0)), (360, 450), 130)
+                # outer_circle = pygame.draw.circle(self.screen, ((255,255,255)), (360, 450), 132)
+                # inner_circle = pygame.draw.circle(self.screen, ((0,0,0)), (360, 450), 130)
+                inner_rect, outer_rect = pygame.Rect(0,0,500,100), pygame.Rect(0,0,495,105)
+                inner_rect.center, outer_rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/6), (SCREEN_WIDTH/2, SCREEN_HEIGHT/6) 
+                pygame.draw.rect(self.screen, 'white', inner_rect)
+                pygame.draw.rect(self.screen, 'black', outer_rect)
                 self.screen.blit(self.game_over_surf, self.game_over_rect)
+                self.screen.blit(self.prev_score_surf, self.prev_score_rect)
                 self.screen.blit(self.play_msg_surf, self.play_msg_rect)
                 if self.score!= 0: self.display_score(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
--- sprites.py
+++ sprites.py
@@ -22,6 +22,8 @@ class Planet(pygame.sprite.Sprite):
         self.image = pygame.image.load('assets/images/assetDarkPlanet.png').convert_alpha()
         self.rect = self.image.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
+        self.mask = pygame.mask.from_surface(self.image)
+
     def rotate(self):
         rotated_img = pygame.transform.rotozoom(self.image, 0.002, 1) 
         self.image = rotated_img
@@ -30,128 +32,168 @@ class Planet(pygame.sprite.Sprite):
         # self.rotate()
         return
 
-# class Tower(pygame.sprite.Sprite):
-#     def __init__(self, groups):
-#         super().__init__(groups)
+class Tower(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
 
-#         self.image = pygame.image.load('assets/images/assetDarkTower.png').convert_alpha()
-#         self.image = pygame.transform.scale(self.image, (80, 240))
-#         self.rect = self.image.get_rect(center = (SCREEN_WIDTH/2, SCREEN_WIDTH/2))
+        self.image = pygame.image.load('assets/images/assetDarkTower.png').convert_alpha()
+        self.image = pygame.transform.smoothscale_by(self.image, 0.5)#pygame.transform.scale(self.image, (100, 250))
+        # self.image = pygame.transform.rotate(self.image, randint(0,360))
+        self.rect = self.image.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 * 0.7))
+        self.mask = pygame.mask.from_surface(self.image)
 
-#         self.angle = 0.032
-#         self.angle_incr = 0.016
-#         self.speed_multiplier = 10
+        self.angle = 0.016
+        self.angle_incr = 0.008
+        self.speed_multiplier = 5
 
-#     def move(self, dt):
-#         self.rect.move_ip(((int(math.cos(self.angle) * self.speed_multiplier)),  (int(math.sin(self.angle) * self.speed_multiplier))))
-#         self.angle += self.angle_incr
-#         return
+    def move(self, dt):
+        self.rect.move_ip(((int(math.cos(self.angle) * self.speed_multiplier)),  (int(math.sin(self.angle) * self.speed_multiplier))))
+        self.angle += self.angle_incr
+        self.mask = pygame.mask.from_surface(self.image)
     
-#     def rotate(self,):
-#         rotated_img = pygame.transform.rotozoom(self.image, self.angle, 1)
-#         self.image = rotated_img
-        
+    def rotate(self, dt):
+        rotated_img = pygame.transform.rotozoom(self.image, self.angle*dt, 1)
+        self.image = rotated_img
+        self.rect = self.image.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2 * 0.7))
+        self.mask = pygame.mask.from_surface(self.image)
 
-#     def change_height(self, dt):
-#         return
+    def change_height(self, dt):
+        return
     
-#     def destroy(self, dt):
-#         self.kill()
+    def destroy(self, dt):
+        self.kill()
 
-#     def update(self, dt):
-#         self.rotate()
-#         return
+    def update(self, dt):
+        # self.move(dt)
+        # self.rotate(dt)
+        return
 
-# class Meteor(pygame.sprite.Sprite):
-#     def __init__(self, groups):
-#         super().__init__(groups)
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
 
-#         self.image = pygame.image.load('assets/images/assetMeteor.gif').convert_alpha()
-#         self.image = pygame.transform.scale(self.image, (50, 50))
-#         self.rect = self.image.get_rect()
+        img1 = pygame.image.load('assets/images/assetMeteor1.png').convert_alpha()
+        img1 = pygame.transform.scale(img1, (50, 50)) 
+        img2 = pygame.image.load('assets/images/assetMeteor2.png').convert_alpha()
+        img2 = pygame.transform.scale(img2, (50, 50)) 
+        self.meteor_frames = [img1,img2]
+        self.index = 0
 
-#     def move(self, dt):
-#         self.rect.x += 2.5
-#         self.rect.y += 2.5
+        self.image = self.meteor_frames[self.index]
+        self.image = pygame.transform.scale(self.image, (50, 50))
+        self.rect = self.image.get_rect(center = (-50,randint(-200, 300)))
 
-#     def destroy(self,dt):
-#         if self.rect.x > SCREEN_WIDTH and self.rect.y > SCREEN_HEIGHT:
-#             self.kill()
-#             print('metoer deleted.')
+        self.mask = pygame.mask.from_surface(self.image)
 
-#     def update(self, dt):
-#         self.move(dt)
-#         self.destroy(dt)
+    def animate(self, dt):
+        self.index += 0.09
+        if self.index >= len(self.meteor_frames):
+            self.index = 0
+        self.image = self.meteor_frames[int(self.index)]
+        self.mask = pygame.mask.from_surface(self.image) # update mask
+
+    def move(self, dt):
+        self.rect.x += 2
+        self.rect.y += 2
+        self.mask = pygame.mask.from_surface(self.image)
+
+    # TODO: delete later
+    def reset_pos(self, dt):
+        if self.rect.x > SCREEN_WIDTH or self.rect.y > SCREEN_HEIGHT:
+            self.rect.center = (-50,-50)
+
+    def destroy(self,dt):
+        if self.rect.x > SCREEN_WIDTH and self.rect.y > SCREEN_HEIGHT:
+            self.kill()
+            print('meteor deleted.')
+
+    def update(self, dt):
+        self.move(dt)
+        self.animate(dt)
+        # self.reset_pos(dt)
+        self.destroy(dt)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
 
         img = pygame.image.load('assets/images/assetMoon.png').convert_alpha()
+        img =  pygame.transform.scale(img, (32,32))
 
-        self.image = pygame.transform.scale(img, (32,32))
+        self.image = img
         self.rect = self.image.get_rect(midbottom = (SCREEN_WIDTH/2, 150))
 
-        self.lives = 3
-        self.score= 0
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # self.score = 0
+        # self.lives = 3
 
         self.player_angle = 0
-        self.angle_incr = 0.016 # angle is incremented by this value each frame
+        self.angle_incr = 0.006 # angle is incremented by this value each frame
+        self.player_gravity = 0.5 # the pull towards the center
+        self.player_rotation = 0.1
         self.player_speed_multiplier = 5 # the speed of the orbital
-        self.player_gravity = 0.01 # the pull towards the center
-        self.player_rotation = 0
-
-    def check_position(self, dt):
-        if self.rect.top > SCREEN_HEIGHT or self.rect.bottom < 0 or self.rect.left > SCREEN_WIDTH+20 or self.rect.right < -20: # Note: SCREEN_HEIGHT is the BOTTOM of the window.
-            self.respawn(dt)
-            print('collision @ screen border')
+        self.player_max_speed = 12 # max orbital speed
     
     def move(self, dt):
         x_coords = (int(math.cos(self.player_angle) * self.player_speed_multiplier))
         y_coords = (int(math.sin(self.player_angle) * self.player_speed_multiplier))
         self.rect.move_ip((x_coords, y_coords))
-        self.player_angle += self.angle_incr
+        self.player_angle += dt
 
-    def respawn(self, dt):
+    def reset_speed(self):
+        self.player_speed_multiplier = 5
+        self.player_gravity = 0.5
+    
+    def increase_speed(self, dt):
+        if self.player_speed_multiplier <= self.player_max_speed:
+            self.player_speed_multiplier += dt/3
+            # self.player_angle += dt * self.player_gravity
+            self.player_gravity += dt/12
+            self.angle_incr *= 1.75
+        # print(self.player_speed_multiplier) if self.player_speed_multiplier >= self.player_max_speed else{}
+            # print(self.player_speed_multiplier)
+
+    def respawn(self):
         self.rect.midbottom = (SCREEN_WIDTH/2-20, 150)
-        self.lives -= 1
+        # self.lives -= 1
+        # print(f'Lives left: {self.lives}')
         self.player_angle = 0
-        print(f'Lives left: {self.lives}')
 
-    def rotate(self, img, x, y):
-        rotated_img = pygame.transform.rotozoom(self.image, self.player_rotation, 1)
+    def rotate(self, dt, img, x, y):
+        rotated_img = pygame.transform.rotozoom(self.image, -dt, 1)
         self.image = rotated_img
 
         # self.image = pygame.transform.rotate(img, self.player_rotation)
         # self.rect =  self.image.get_rect(center = img.get_rect(center = (x, y)).center)
 
-        self.player_rotation += 0.01
+        # self.player_rotation += 0.1
+        self.mask = pygame.mask.from_surface(self.image) # update mask
     
     def check_input(self, dt):
         key = pygame.key.get_pressed()
         if key[pygame.K_SPACE]:
             self.player_angle -= 0.03
-            self.score +=1
         # if key[pygame.K_a]:
         #     self.rect.x -= 3
         # if key[pygame.K_d]:
         #     self.rect.x += 3
 
     def gravity(self, dt):
-        self.player_angle += self.player_gravity
+        self.player_angle += self.player_gravity * dt
 
-    def get_score(self, dt):
-        return self.score
+    # def get_score(self, dt):
+    #     return self.score
     
-    def get_lives(self, dt):
-        return self.lives
+    # def get_lives(self, dt):
+    #     return self.lives
 
     def update(self, dt):
-        self.check_position(dt)
         self.move(dt)
+        self.increase_speed(dt)
         self.check_input(dt)
         self.gravity(dt)
-        # self.rotate(self.image, self.rect.x, self.rect.y)
+        # self.rotate( dt, self.image,self.rect.x, self.rect.y)
     
-    def destroy():
-        Player.kill() # ~ 
+    def destroy(self):
+        self.kill() # ~ 
